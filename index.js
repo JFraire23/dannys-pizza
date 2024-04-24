5bd1752c7b01821fa9844fca526956aebfa7163f
JFraire23 <148163384+JFraire23@users.noreply.github.com>
2024-04-24 15:18:11 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,678 @@
+//Array Principal
+
+const platillos = [
+	//PIZZAS
+
+	{
+		id: "Pizza Pepperoni Chica",
+		titulo: "Pizza Pepperoni Chica", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/pepperoni.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: pepperoni y queso.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 100
+	},
+
+	{
+		id: "Pizza Pepperoni Mediana",
+		titulo: "Pizza Pepperoni Mediana", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/pepperoni.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: pepperoni y queso.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 150
+	},
+
+	{
+		id: "Pizza Pepperoni Grande",
+		titulo: "Pizza Pepperoni Grande", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/pepperoni.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: pepperoni y queso.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 200
+	},
+
+	{
+		id: "Pizza Hawaiana Chica",
+		titulo: "Pizza Hawaiana Chica", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/hawaiana.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: jamón, queso y piña.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 100
+	},
+
+	
+	{
+		id: "Pizza Hawaiana Mediana",
+		titulo: "Pizza Hawaiana Mediana", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/hawaiana.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: jamón, queso y piña.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 150
+	},	
+
+	{
+		id: "Pizza Hawaiana Grande",
+		titulo: "Pizza Hawaiana Grande", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/hawaiana.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: jamón, queso y piña.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 200
+	},
+
+	{
+		id: "Pizza Mexicana Chica",
+		titulo: "Pizza Mexicana Chica", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/mexicana.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: chorizo, queso, tocino y chile jalapeño.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 100
+	},
+
+	{
+		id: "Pizza Mexicana Mediana",
+		titulo: "Pizza Mexicana Mediana", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/mexicana.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: chorizo, queso, tocino y chile jalapeño.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 150
+	},
+
+	{
+		id: "Pizza Mexicana Grande",
+		titulo: "Pizza Mexicana Grande", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/mexicana.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: chorizo, queso, tocino y chile jalapeño.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 200
+	},
+
+	
+	{
+		id: "Pizza 3Carnes Chica",
+		titulo: "Pizza 3Carnes Chica", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/3carnes.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: queso, pepperoni, jamón y tocino.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 140
+	},	
+
+	{
+		id: "Pizza 3Carnes Mediana",
+		titulo: "Pizza 3Carnes Mediana", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/3carnes.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: queso, pepperoni, jamón y tocino.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 160
+	},
+
+	{
+		id: "Pizza 3Carnes Grande",
+		titulo: "Pizza 3Carnes Grande", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/3carnes.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: queso, pepperoni, jamón y tocino.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 200
+	},
+
+	{
+		id: "Pizza Suprema Chica",
+		titulo: "Pizza Suprema Chica", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/suprema.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: pepperoni, pimiento morrón, aceituna y champiñones. 8 rebanadas",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 140
+	},
+
+	{
+		id: "Pizza Suprema Mediana",
+		titulo: "Pizza Suprema Mediana", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/suprema.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: pepperoni, pimiento morrón, aceituna y champiñones.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 160
+	},
+
+	{
+		id: "Pizza Suprema Grande",
+		titulo: "Pizza Suprema Grande", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/suprema.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: pepperoni, pimiento morrón, aceituna y champiñones.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 200
+	},
+
+	{
+		id: "Pizza Al Pastor Chica",
+		titulo: "Pizza Al Pastor Chica", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/alpastor.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: carne adobada, pepperoni y piña.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 140
+	},
+
+	{
+		id: "Pizza Al Pastor Mediana",
+		titulo: "Pizza Al Pastor Mediana", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/alpastor.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: carne adobada, pepperoni y piña.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 160
+	},
+
+	{
+		id: "Pizza Al Pastor Grande",
+		titulo: "Pizza Al Pastor Grande", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/alpastor.png", /*ruta de la imagen*/
+		contenido: "Ingredientes: carne adobada, pepperoni y piña.",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 200
+	},
+
+	//PROMOS
+
+	{
+		id: "PROMO Combo Mix",
+		titulo: "Promo Combo Mix", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/combo.png", /*ruta de la imagen*/
+		contenido: "1 pizza chica de pepperoni, refresco de 1.5l, 1 orden de boneless y 1 orden de papas a la francesa",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PROMOCIONES",
+			id: "promociones"	
+		},
+		precio: 230
+	},
+
+	{
+		id: "Papas a la Francesa",
+		titulo: "Papas a la Francesa", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/papas.jpg", /*ruta de la imagen*/
+		contenido: "Porción de papas a la francesa",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "PIZZAS",
+			id: "pizzas"	
+		},
+		precio: 40
+	},
+
+
+	//COMPLEMENTOS
+
+	{
+		id: "Alitas Bbq",
+		titulo: "Alitas Bbq", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/alitas.png", /*ruta de la imagen*/
+		contenido: "300gr de Alitas en salsa Bbq, porcion de zanahoria, apio y ranch",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "ALITAS",
+			id: "alitas"	
+		},
+		precio: 120
+	},
+
+	{
+		id: "Alitas Búfalo",
+		titulo: "Alitas Búfalo", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/alitas.png", /*ruta de la imagen*/
+		contenido: "300gr de Alitas en salsa Búfalo, porcion de zanahoria, apio y ranch",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "ALITAS",
+			id: "alitas"	
+		},
+		precio: 120
+	},
+
+	{
+		id: "Alitas Habanero",
+		titulo: "Alitas Habanero", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/alitas.png", /*ruta de la imagen*/
+		contenido: "300gr de Alitas en salsa Bbq, porcion de zanahoria, apio y ranch",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "ALITAS",
+			id: "alitas"	
+		},
+		precio: 120
+	},
+
+	{
+		id: "Boneless Bbq",
+		titulo: "Boneless Bbq", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/boneless-bbq.jpeg", /*ruta de la imagen*/
+		contenido: "300gr Boneless en salsa Bbq, porcion de zanahoria, apio y ranch",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "BONELESS",
+			id: "boneless"	
+		},
+		precio: 120
+	},
+
+	{
+		id: "Boneless Búfalo",
+		titulo: "Boneless Búfalo", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/boneless-bufalo.jpeg", /*ruta de la imagen*/
+		contenido: "300gr Boneless en salsa Búfalo, porcion de zanahoria, apio y ranch",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "BONELESS",
+			id: "boneless"	
+		},
+		precio: 120
+	},
+
+	{
+		id: "Boneless Habanero",
+		titulo: "Boneless Habanero", /*este titulo se va a ver en el dom*/
+		titulo1: "",
+		titulo2: "",
+		titulo3: "",
+		titulo4: "",
+		titulo5: "",
+		imagen: "./imagenes/desayunos/imagenes-tamaño/boneless-habanero.jpeg", /*ruta de la imagen*/
+		contenido: "300gr Boneless en salsa Habanero, porcion de zanahoria, apio y ranch",
+		cocina: "dannys pizzas",
+		categoria: {
+			nombre: "BONELESS",
+			id: "boneless"	
+		},
+		precio: 120
+	},
+
+];
+
+const contenedorImagenes = document.querySelector("#contenedor-imagenes");//imagenes=platillos
+const botonesCategorias = document.querySelectorAll(".boton-clases");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".boton-agregar");
+const numerito = document.querySelector("#numerito");
+//let barraBusqueda = document.querySelector("#buscador");
+
+function cargarPlatillos(platillosElegidos) {
+
+	contenedorImagenes.innerHTML = "";
+
+	platillosElegidos.forEach(platillo => {
+		const div = document.createElement("div");
+		div.classList.add("platillo");
+		div.innerHTML = `
+
+
+			<div class="platillo">
+				<div class="platillo-detalles">
+					<div class="imagen">
+						<img src="${platillo.imagen}">
+					</div>
+					<p class="platillo-titulo">${platillo.titulo}</p>
+					<div class="descripcion">
+						
+						<p>${platillo.contenido}</p>
+						
+					</div>
+					
+					<div class="precio">
+						
+						<button class="boton-precio">$${platillo.precio}</button>
+					</div>
+
+					<div class="agregar-carrito">
+						<button class="boton-agregar" id="${platillo.id}">Agregar Al Carrito</button>
+					</div>
+					
+				</div>
+				
+			</div>
+				
+		`;
+		contenedorImagenes.append(div);
+
+
+
+	})
+
+	actualizarBotonesAgregar();
+
+} 
+
+/*function buscarArticulos() {
+	buscador.addEventListener("input", e => {
+		const inputText = e.target.value.toLowerCase().trim();
+		console.log(inputText);
+		
+		const mostrarFiltrado = platillos.filter(platillo => 
+			platillo.titulo.toLowerCase().startsWith(inputText)||
+			platillo.cocina.toLowerCase().startsWith(inputText)||
+			platillo.contenido.toLowerCase().startsWith(inputText)||
+			platillo.titulo1.toLowerCase().startsWith(inputText)||
+			platillo.titulo2.toLowerCase().startsWith(inputText)||
+			platillo.titulo3.toLowerCase().startsWith(inputText)||
+			platillo.titulo4.toLowerCase().startsWith(inputText)||
+			platillo.titulo5.toLowerCase().startsWith(inputText)||
+			platillo.precio.toString().startsWith(inputText)
+			)
+			;
+
+		
+		cargarPlatillos(mostrarFiltrado)
+	})
+
+}
+*/
+cargarPlatillos(platillos);
+//buscarArticulos();
+
+botonesCategorias.forEach(boton => {
+	boton.addEventListener("click", (e) => {
+
+		botonesCategorias.forEach(boton => {
+			boton.classList.remove("active")
+		})
+		e.currentTarget.classList.add("active");
+
+		if (e.currentTarget.id !=  "todos") {
+			const platilloCategoria = platillos.find(platillo => platillo.categoria.id === e.currentTarget.id);
+
+			tituloPrincipal.innerText = platilloCategoria.categoria.nombre;
+			const platillosBoton = platillos.filter(platillo => platillo.categoria.id === e.currentTarget.id);
+
+
+		cargarPlatillos(platillosBoton);
+		} else {
+			tituloPrincipal.innerText = "Todos los platillos";
+			cargarPlatillos(platillos);
+		}
+	})
+});
+
+function actualizarBotonesAgregar(){
+	botonesAgregar = document.querySelectorAll(".boton-agregar");
+
+	botonesAgregar.forEach(boton => {
+		boton.addEventListener("click", agregarAlCarrito);
+	});
+}
+
+let platillosEnCarritoLasMarias;
+const platillosEnCarritoLasMariasLS = JSON.parse(localStorage.getItem("platillos-Dannys-Pizza"));
+if (platillosEnCarritoLasMariasLS) {
+	platillosEnCarritoLasMarias = platillosEnCarritoLasMariasLS;
+	actualizarNumerito();
+}
+else{
+	platillosEnCarritoLasMarias = [];
+}
+ 
+
+function agregarAlCarrito(e) {
+	Toastify({
+  		text: "platillo agregado al carrito",
+  		duration: 2000,
+  		close: true,
+  		gravity: "top", // `top` or `bottom`
+  		position: "right", // `left`, `center` or `right`
+  		stopOnFocus: true, // Prevents dismissing of toast on hover
+  		style: {
+    		background: "linear-gradient(to right, #FF4C00, yellow)",
+    		borderRadius: "2rem",
+    		fontSize: '1rem',
+  		},
+  		onClick: function(){} // Callback after click
+		}).showToast();
+
+	const idBoton = e.currentTarget.id;
+	const platilloAgregado = platillos.find(platillo => platillo.id === idBoton);
+
+	if (platillosEnCarritoLasMarias.some(platillo => platillo.id === idBoton)) {
+		const index = platillosEnCarritoLasMarias.findIndex(platillo => platillo.id === idBoton);
+		platillosEnCarritoLasMarias[index].cantidad++;
+
+	}
+	else{
+		platilloAgregado.cantidad = 1;
+		platillosEnCarritoLasMarias.push(platilloAgregado);	
+	}
+
+	actualizarNumerito();
+
+		localStorage.setItem("platillos-Dannys-Pizza", JSON.stringify(platillosEnCarritoLasMarias));
+
+}
+
+function actualizarNumerito(){
+	let nuevoNumerito = platillosEnCarritoLasMarias.reduce((acc, platillo)=> acc + platillo.cantidad, 0);
+	numerito.innerText = nuevoNumerito;
+}
+
+window.onload = actualizarPagina();
+
+        function actualizarPagina() {
+            let actualizar = false;
+            momentoActual = new Date();
+            hora = momentoActual.getHours();
+            minuto = momentoActual.getMinutes();
+            segundo = momentoActual.getSeconds();
+
+            str_segundo = new String (segundo);
+            if (str_segundo.length == 1) {
+                segundo = "0" + segundo;
+            }
+            str_minuto = new String (minuto);
+            if (str_minuto.length == 1){
+                minuto = "0" + minuto;
+            }
+            str_hora = new String (hora);
+            if (str_hora.length == 1){
+                hora = "0" + hora;
+            }
+            horaImprimible = hora + ":" + minuto + ":" + segundo;
+            if(horaImprimible == "12:49:00") {
+                actualizar = true;
+            }
+            setTimeout("actualizarPagina()",1000);
+            if(actualizar == true) {//Comprueba que la hora es igual a la que quieres y actualiza
+                location.reload();
+            }
+        }
+
+
+
+
